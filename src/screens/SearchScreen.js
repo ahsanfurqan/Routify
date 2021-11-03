fc80827d8caa64c0b347f81643e8819ebc21da6f
ahsanfurqan <ahsanfurqan.bukc@gmail.com>
2021-11-03 22:20:12 +0500

    search filter

--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -1,20 +1,119 @@
-import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import React, { useState, useEffect } from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  FlatList,
+  SafeAreaView,
+  Dimensions,
+  TextInput,
+} from "react-native";
 import { Stops } from "../../Data/stop";
+import { useSelector, useDispatch } from "react-redux";
+import { setDestination, selectDestination } from "../../slices/navSlice";
+
+const width = Dimensions.get("window").width;
+const height = Dimensions.get("window").height;
 
 export default function SearchScreen() {
+  const [search, setSearch] = useState("");
+  const [filteredDataSource, setFilteredDataSource] = useState([]);
+  const [masterDataSource, setMasterDataSource] = useState([]);
+  // dispatch to send data to redux
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    setFilteredDataSource(Stops);
+    setMasterDataSource(Stops);
+  }, []);
+
+  const searchFilterFunction = (text) => {
+    // Check if searched text is not blank
+    if (text) {
+      // Inserted text is not blank
+      // Filter the masterDataSource
+      // Update FilteredDataSource
+      const newData = masterDataSource.filter(function (item) {
+        const itemData = item.title
+          ? item.title.toUpperCase()
+          : "".toUpperCase();
+        const textData = text.toUpperCase();
+        return itemData.indexOf(textData) > -1;
+      });
+      setFilteredDataSource(newData);
+      setSearch(text);
+    } else {
+      // Inserted text is blank
+      // Update FilteredDataSource with masterDataSource
+      setFilteredDataSource(masterDataSource);
+      setSearch(text);
+    }
+  };
+
+  const ItemView = ({ item }) => {
+    return (
+      // Flat List Item
+      <Text style={styles.itemStyle} onPress={() => getItem(item)}>
+        {item.title.toUpperCase()}
+      </Text>
+    );
+  };
+
+  const ItemSeparatorView = () => {
+    return (
+      // Flat List Item Separator
+      <View
+        style={{
+          height: 0.5,
+          width: "100%",
+          backgroundColor: "#C8C8C8",
+        }}
+      />
+    );
+  };
+  // const destination = useSelector(selectDestination);
+  const getItem = (item) => {
+    // Function for click on an item
+    dispatch(setDestination(item));
+    // alert("Id : " + item.key + " Title : " + item.title);
+    // const destination = useSelector(selectDestination);
+    // console.log(destination);
+  };
+
   return (
-    <View style={styles.container}>
-      <Text>search screen</Text>
-    </View>
+    <SafeAreaView style={{ flex: 1 }}>
+      <View style={styles.container}>
+        <TextInput
+          style={styles.textInputStyle}
+          onChangeText={(text) => searchFilterFunction(text)}
+          value={search}
+          underlineColorAndroid="transparent"
+          placeholder="Where to go?"
+        />
+        <FlatList
+          data={filteredDataSource}
+          keyExtractor={(item, index) => index.toString()}
+          ItemSeparatorComponent={ItemSeparatorView}
+          renderItem={ItemView}
+        />
+      </View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
+    backgroundColor: "white",
+  },
+  itemStyle: {
+    padding: 10,
+  },
+  textInputStyle: {
+    height: 40,
+    borderWidth: 1,
+    paddingLeft: 20,
+    margin: 40,
+    borderColor: "#009688",
+    backgroundColor: "#FFFFFF",
   },
 });
